7600d8eaba7fd32fb14f1d7db2f0ea5d33145299
Chris Hua <hua.christopher@gmail.com>
2024-03-05 17:23:32 -0500

    fix: oops

--- gazelle/__init__.py
+++ gazelle/__init__.py
@@ -1,6 +1,9 @@
-from modeling_gazelle import (
+"""Gazelle is a joint speech-language model for end-to-end voice conversation."""
+from .modeling_gazelle import (
     GazelleConfig,
     GazelleForConditionalGeneration,
     GazellePreTrainedModel,
     GazelleProcessor,
 )
+
+__version__ = "0.1.0"
